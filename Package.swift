f6f767942f5675b5fc08b01126c9a5e959c866e5
Semen Osipov <semen.osipov@arealidea.ru>
2022-12-03 20:25:30 +0300

    fix SPM

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Blurberry"),
+            name: "Blurberry",
+            dependencies: ["NSObject+Extension"]),
         .target(
             name: "NSObject+Extension",
-            dependencies: ["Blurberry"],
             path: "Sources",
             exclude: ["Blurberry"])]
 )
